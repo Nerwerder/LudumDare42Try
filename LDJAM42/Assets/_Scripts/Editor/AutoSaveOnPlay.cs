8b22d530cfaf2912ff44f7c6b3d423cfb975ba33
Harker <kilian.roth@stud.hs-kempten.de>
2018-08-10 11:30:35 +0200

    Created Unity project with correct settings and added autosave on play editorscript

---
+++ LDJAM42/Assets/_Scripts/Editor/AutoSaveOnPlay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+
+[InitializeOnLoad]
+public class AutoSaveOnPlay : ScriptableObject
+{
+    static AutoSaveOnPlay()
+    {
+        EditorApplication.playModeStateChanged += (PlayModeStateChange state) => 
+            {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                Debug.Log("Auto-saving all open scenes...");
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
