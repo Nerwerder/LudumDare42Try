cb6931759439de14c592dd42e258b1244786a527
Sebastian <Spolwind.Sebastian@googlemail.com>
2018-08-10 23:06:02 +0200

    GizmoDraw

---
+++ LDJAM42/Assets/_Scripts/Help/GizmoDraw.cs
@@ -0,0 +1,18 @@
+﻿//@ Sebastian
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Simple Helper-Script for Drawing Gizmos in the Editor (not visible inGame)
+public class GizmoDraw : MonoBehaviour {
+
+    public Color gizmoColor = Color.yellow;
+    public float gizmoSize = 0.1f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawSphere(transform.position, gizmoSize);
+    }
+}
