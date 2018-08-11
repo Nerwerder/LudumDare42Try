78022be0b23ac63aec98c849974a39165f855d1b
Sebastian <Spolwind.Sebastian@googlemail.com>
2018-08-11 12:48:30 +0200

    Building Buildings: City
    City is done

---
+++ LDJAM42/Assets/_Scripts/Buildings/Buildings.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildings : MonoBehaviour
+{
+    public enum BuildingType { city, lumberjack, sawmill, farm, windmill, bakery }
+    public List<GameObject> buildingPrefabs;
+
+    private List<Building> buildings;
+
+    //Things the System builds
+    public bool defaultBuild(int t, Place p)
+    {
+        switch (t)
+        {
+            case 3: //trees
+
+                return true;
+            case 4:     //City
+                var cG = p.buildBuilding(buildingPrefabs[0], this.transform);
+                var city = cG.AddComponent<City>();
+                buildings.Add(city);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    //Things the Player can Build
+    public bool build(BuildingType t, Place p)
+    {
+        //1. Test the Field
+        if (p.TestForBuilding() == false)
+            return false;
+
+        //2. Build the Building
+        switch (t)
+        {
+            case BuildingType.lumberjack:
+
+                return true;
+
+            case BuildingType.sawmill:
+
+                return true;
+
+            case BuildingType.farm:
+
+                return true;
+
+            case BuildingType.windmill:
+
+                return true;
+
+            case BuildingType.bakery:
+
+                return true;
+        }
+
+
+        return false;
+    }
+
+}
--- LDJAM42/Assets/_Scripts/World/Place.cs
+++ LDJAM42/Assets/_Scripts/World/Place.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Place : MonoBehaviour
 {
-    private int line, column, type;
+    public enum PlaceType { Water, Meadow, Forest, City}
+
+    PlaceType type;
+    private int line, column;
+    private bool free;
 
     public void Set(int t, int x, int z)
     {
-        type = t;
+        free = true;
+        type = (PlaceType)t;
         SetPos(x, z);
     }
 
@@ -31,6 +36,19 @@ public class Place : MonoBehaviour
     public Vector3 GetPos()
     {
         return GetComponent<GameObject>().transform.position;
+    }
+
+    public bool TestForBuilding()
+    {
+        if (type == PlaceType.Meadow && free == true)
+            return true;
+        return false;
+    }
+
+    public GameObject buildBuilding(GameObject g, Transform parent)
+    {
+        free = false;
+        return Instantiate(g, (this.transform.position + g.transform.position), this.transform.rotation, parent);
     }
 
 }
