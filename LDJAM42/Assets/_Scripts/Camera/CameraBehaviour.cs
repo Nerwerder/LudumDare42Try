519b38d12ebceeeeda9a3cbe52d7587800d9d390
Unknown <Spolwind.Sebastian@googlemail.com>
2018-08-11 11:16:00 +0200

    basic Camera Control
    Move, Rotate, Zoom and resetRotation.

--- LDJAM42/Assets/_Scripts/Camera/CameraBehaviour.cs
+++ LDJAM42/Assets/_Scripts/Camera/CameraBehaviour.cs
@@ -5,23 +5,83 @@ using UnityEngine;
 public class CameraBehaviour : MonoBehaviour
 {
     public float startHeight = 10f;
+    public float zoomSpeed = 2f;
+    public float movementSpeed = 0.5f;
+    public float rotationSpeed = 1f;
+
+    public bool rotation = true;
 
     private World world;
+    private bool rightButton, middleButton, rButton;
+    private float xMovement, yMovement, wZoom;
+
+    private Vector3 startPosition, startLookAt;
+    private Quaternion startRotation;
+    
 
 	// Use this for initialization
 	void Start ()
     {
         world = FindObjectOfType<WorldCreation>().GetWorld();
-        var pos = new Vector3(world.getSizeX() / 2, startHeight, -(world.getSizeY()/2));
-        var pos2 = pos;
-        pos2.y = 0;
+        startPosition = new Vector3(world.getSizeX() / 2, startHeight, -(world.getSizeY()/2));
+        startLookAt = new Vector3(startPosition.x, 0, startPosition.z);
 
-        this.transform.position = pos;
-        this.transform.LookAt(pos2);
+        resetCamera();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    public void SetInput(bool right, bool middle, float x, float y, float w, bool bR)
+    {
+        rightButton = right;
+        middleButton = middle;
+
+        xMovement = x;
+        yMovement = y;
+        wZoom = w;
+
+        rButton = bR;
+    }
+
+    void Update ()
+    {
+        if (wZoom != 0f)
+            Zoom(wZoom);
+
+        if (rightButton)
+            Movement(xMovement, yMovement);
+        else if (middleButton && rotation)
+            Rotation(xMovement, yMovement);
+
+        if (rButton)
+            resetCameraRotation();
+
 	}
+
+    private void Zoom(float z)
+    {
+        this.transform.position += this.transform.forward * z * zoomSpeed;
+    }
+
+    private void Movement(float x, float y)
+    {
+        this.transform.position -= this.transform.right * x * movementSpeed;
+        this.transform.position -= this.transform.up * y * movementSpeed;
+    }
+
+    private void Rotation(float x, float y)
+    {
+        this.transform.Rotate(this.transform.right, y * rotationSpeed);
+        this.transform.Rotate(Vector3.up, -x * rotationSpeed);
+    }
+
+    private void resetCamera()
+    {
+        this.transform.position = startPosition;
+        this.transform.LookAt(startLookAt);
+        startRotation = this.transform.rotation;
+    }
+
+    private void resetCameraRotation()
+    {
+        this.transform.rotation = startRotation;
+    }
 }
